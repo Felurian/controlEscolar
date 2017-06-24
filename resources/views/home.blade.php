b6f23cb5ad76bc24e34d8f25a208d019399d990a
edithmenb <edith.mendoza.barraza@gmail.com>
2017-06-24 15:54:30 -0600

    probando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('contenido')
 <div class="jumbotron">
   <h1>Sistema de Control Escolar</h1>
-  <p>Este sistema permite administrar los procesos relacionados con control escolar. Permite el registro de alumnos, materias, maestros, horarios, así como también la carga de materias.</p>
+  <p>ESTE SISSSTEMAAAA Este sistema permite administrar los procesos relacionados con control escolar. Permite el registro de alumnos, materias, maestros, horarios, así como también la carga de materias.</p>
   <p><a class="btn btn-primary btn-lg">Mas...</a></p>
 </div>
 @stop
