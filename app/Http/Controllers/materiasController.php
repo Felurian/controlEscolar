c7b4833f667235527c43e2237fdf56daeda093b3
Felurian <Felurian@users.noreply.github.com>
2017-06-24 18:03:11 -0600

    materiasController

---
+++ app/Http/Controllers/materiasController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Materias;
+use DB;
+
+class materiasController extends Controller
+{
+   
+   public function registrar(){
+         
+         return view('registrarMateria');
+   }
+
+   public function guardar(Request $datos){
+         $materia= new Materias();
+         $materia->nombre=$datos->input('nombre');
+         $materia->save();
+
+         return redirect('/consultarMaterias');
+   }
+   public function consultar(){
+         $materias=DB::table('materias')
+            ->select('materias.*')
+            ->paginate(5);
+
+         return view('consultarMaterias', compact('materias'));
+   }
+   public function eliminar($id){
+         $materia=Materias::find($id);
+         $materia->delete();
+         return redirect('consultarMaterias');
+   }
+   public function editar($id){
+      $materia=DB::table('materias')
+         ->where('materias.id', '=', $id)
+         ->select('materias.*')
+         ->first();
+
+      return view('editarMaterias', compact('materia'));
+   }
+   public function actualizar($id, Request $datos){
+      $materia=Materias::find($id);
+      $materia->nombre=$datos->input('nombre');
+      $materia->save();
+
+      return redirect('consultarMaterias');
+   }  
+   
+}
+
+
+
+
+
+
+
+
+
+
+
