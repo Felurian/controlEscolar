78d68f4d7fef40b5e30a141625da26d8edff7cc0
Felurian <Felurian@users.noreply.github.com>
2017-06-24 18:49:42 -0600

    respaldo DB

--- controlescolar.sql
+++ controlescolar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 25, 2017 at 02:20 AM
+-- Generation Time: Jun 25, 2017 at 02:49 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
